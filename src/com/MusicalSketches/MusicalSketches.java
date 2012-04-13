b481d9375c84bc21cde2e6dc88921f53aa766efe
Stephen Freiberg <safreiberg@gmail.com>
2012-04-13 14:44:32 -0400

    hello world complete

--- src/com/MusicalSketches/MusicalSketches.java
+++ src/com/MusicalSketches/MusicalSketches.java
@@ -2,12 +2,15 @@ package com.MusicalSketches;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MusicalSketches extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android. Welcome to Musical Sketches, brought to you by Exploding Flowers!");
+        setContentView(tv);
     }
 }
