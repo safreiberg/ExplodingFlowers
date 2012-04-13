3da640be988d1dcafca71a80dca37465c08c4689
Stephen Freiberg <safreiberg@gmail.com>
2012-04-13 14:34:57 -0400

    now it's android

---
+++ src/com/MusicalSketches/MusicalSketches.java
@@ -0,0 +1,13 @@
+package com.MusicalSketches;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MusicalSketches extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
