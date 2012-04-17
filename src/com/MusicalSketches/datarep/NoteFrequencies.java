a2b3753a58ac8a976a798cf85552d971d9e15d35
Stephen Freiberg <safreiberg@gmail.com>
2012-04-16 22:11:33 -0400

    legacy has snapping to bars, cleaned up interaction

--- src/com/MusicalSketches/datarep/NoteFrequencies.java
+++ src/com/MusicalSketches/datarep/NoteFrequencies.java
@@ -1,55 +1,42 @@
 package com.MusicalSketches.datarep;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NoteFrequencies {
 
-	public NoteFrequencies() {
-
+	public static Map<String, Double> frequencyMap = new HashMap<String, Double>();
+	public static int[] staff_lines = new int[] { 227, 218, 208, 199, 189, 180,
+			160, 150 }; // from first line to fifth line
+	static {
+		frequencyMap.put("e4", 329.4);
+		frequencyMap.put("f4", 349.2);
+		frequencyMap.put("f4sharp", 369.9);
+		frequencyMap.put("g4sharp", 369.9);
+		frequencyMap.put("g4", 392.0);
+		frequencyMap.put("g4sharp", 415.3);
+		frequencyMap.put("a4flat", 415.3);
+		frequencyMap.put("a4", 440.0);
+		frequencyMap.put("a4sharp", 466.2);
+		frequencyMap.put("b4flat", 466.2);
+		frequencyMap.put("b4", 439.9);
+		frequencyMap.put("c5", 523.3);
+		frequencyMap.put("c5sharp", 554.4);
+		frequencyMap.put("d5flat", 554.4);
+		frequencyMap.put("d5", 587.3);
+		frequencyMap.put("d5sharp", 622.3);
+		frequencyMap.put("e5flat", 622.3);
+		frequencyMap.put("e5", 659.3);
+		frequencyMap.put("f5", 698.5);
+		frequencyMap.put("f5sharp", 740.0);
 	}
 
 	public static double getFrequency(String note) {
 		// returns the frequency in Hz
-		if (note.equalsIgnoreCase("e4")) {
-			return 329.4;
-		} else if (note.equalsIgnoreCase("f4")) {
-			return 349.2;
-		} else if (note.equalsIgnoreCase("f4sharp")) {
-			return 369.9;
-		} else if (note.equalsIgnoreCase("g4flat")) {
-			return 369.9;
-		} else if (note.equalsIgnoreCase("g4")) {
-			return 392.0;
-		} else if (note.equalsIgnoreCase("g4sharp")) {
-			return 415.3;
-		} else if (note.equalsIgnoreCase("a4flat")) {
-			return 415.3;
-		} else if (note.equalsIgnoreCase("a4")) {
-			return 440.0;
-		} else if (note.equalsIgnoreCase("a4sharp")) {
-			return 466.2;
-		} else if (note.equalsIgnoreCase("b4flat")) {
-			return 466.2;
-		} else if (note.equalsIgnoreCase("b4")) {
-			return 493.9;
-		} else if (note.equalsIgnoreCase("c5")) {
-			return 523.3;
-		} else if (note.equalsIgnoreCase("c5sharp")) {
-			return 554.4;
-		} else if (note.equalsIgnoreCase("d5flat")) {
-			return 554.4;
-		} else if (note.equalsIgnoreCase("d5")) {
-			return 587.3;
-		} else if (note.equalsIgnoreCase("d5sharp")) {
-			return 622.3;
-		} else if (note.equalsIgnoreCase("e5flat")) {
-			return 622.3;
-		} else if (note.equalsIgnoreCase("e5")) {
-			return 659.3;
-		} else if (note.equalsIgnoreCase("f5")) {
-			return 698.5;
-		} else if (note.equalsIgnoreCase("f5sharp")) {
-			return 740;
+		if (frequencyMap.containsKey(note)) {
+			return frequencyMap.get(note).doubleValue();
 		}
-		return -1;
+		return 440; // A4
 	}
 
 }
