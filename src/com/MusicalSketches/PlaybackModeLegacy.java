cf3e6d19389aa2e46a78fe2cae61a45e39292c5d
Stephen Freiberg <safreiberg@gmail.com>
2012-04-19 10:31:57 -0400

    arrows!!

--- src/com/MusicalSketches/PlaybackModeLegacy.java
+++ src/com/MusicalSketches/PlaybackModeLegacy.java
@@ -1,14 +1,17 @@
 package com.MusicalSketches;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
-import android.content.Intent;
-import android.media.MediaPlayer;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -18,8 +21,6 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.MusicalSketches.EditMode.types;
-import com.MusicalSketches.EditModeLegacy.states;
 import com.MusicalSketches.datarep.Note;
 import com.MusicalSketches.datarep.NoteFrequencies;
 import com.MusicalSketches.datarep.Song;
@@ -28,8 +29,15 @@ public class PlaybackModeLegacy extends Activity {
 	Song song;
 	int notesOnScreen = 0;
 	int state = 0;
-	MediaPlayer mediaPlayer;
+	int sampleRate = 8000;
+	byte[] generatedSnd;
+	int numSamples;
+	AudioTrack audioTrack;
+	ImageButton b;
+	int xloc;
 	ImageView arrow;
+	ViewGroup group;
+	Timer arrowTimer;
 	int[] arrowLocations = new int[] { 110, 210, 310, 410, 510, 610 };
 
 	@Override
@@ -37,41 +45,44 @@ public class PlaybackModeLegacy extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.playback_mode);
 
-		mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
-
 		song = (Song) getIntent().getSerializableExtra("song object");
-		arrow = (ImageView)findViewById(R.id.arrow_indicator);
+		arrow = (ImageView) findViewById(R.id.arrow_indicator);
+
 		addClefMeterKey(song);
+		genTone();
 
 		ImageButton play_pause = (ImageButton) findViewById(R.id.play_pause_button);
 		ImageButton rewind_button = (ImageButton) findViewById(R.id.rewind_button);
 		ImageButton forward_button = (ImageButton) findViewById(R.id.forward_button);
+		b = (ImageButton) findViewById(R.id.play_pause_button);
+		group = (ViewGroup) findViewById(R.id.edit_layout);
 
 		play_pause.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				ImageButton b = (ImageButton) findViewById(R.id.play_pause_button);
 				if (state == 0) {
 					state = 1;
 					b.setImageResource(R.drawable.pause);
-					mediaPlayer.start();
-					playArrows();
+					audioTrack.play();
+					initArrowTimer();
 				} else {
 					state = 0;
 					b.setImageResource(R.drawable.play);
-					mediaPlayer.pause();
+					audioTrack.pause();
 				}
 			}
 		});
 
 		rewind_button.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 5000);
+				audioTrack.stop();
+				makeTrack();
 			}
 		});
 
 		forward_button.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 5000);
+				audioTrack.setPlaybackHeadPosition(audioTrack
+						.getPlaybackHeadPosition());
 			}
 		});
 
@@ -80,7 +91,7 @@ public class PlaybackModeLegacy extends Activity {
 
 	public void uploadFromSong(Song s) {
 		int g = 0;
-		ViewGroup group = (ViewGroup)findViewById(R.id.edit_layout);
+		ViewGroup group = (ViewGroup) findViewById(R.id.edit_layout);
 		for (Note n : s.getNotes().getNotes()) {
 			Log.d("", "adding note");
 			double freq = n.getPitch();
@@ -216,15 +227,12 @@ public class PlaybackModeLegacy extends Activity {
 		switch (item.getItemId()) {
 		case R.id.playback_close:
 			Toast.makeText(this, "Bye!", Toast.LENGTH_SHORT).show();
-			mediaPlayer.stop();
+			audioTrack.stop();
 			finish();
 			break;
 		case R.id.playback_edit:
 			Toast.makeText(this, "As you wish...", Toast.LENGTH_SHORT).show();
-			mediaPlayer.stop();
-			//Intent next = new Intent(PlaybackModeLegacy.this, EditModeLegacy.class);
-			//next.putExtra("song object", song);
-			//startActivity(next);
+			audioTrack.stop();
 			finish();
 			break;
 		}
@@ -240,7 +248,95 @@ public class PlaybackModeLegacy extends Activity {
 		((TextView) findViewById(R.id.meter_text)).setText("" + s.getMeterTop()
 				+ "\n" + s.getMeterBottom());
 	}
-	
-	public void playArrows() {
+
+	public void initArrowTimer() {
+		int g = 0;
+		arrowTimer = new Timer();
+		long time = 0;
+		group.removeView(arrow);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT);
+		params.topMargin = (int) 50;
+		params.leftMargin = 80;
+		arrow.setLayoutParams(params);
+		group.addView(arrow);
+		arrow.setVisibility(0);
+		for (Note n : song.getNotes().getNotes()) {
+			time += n.getLength()*1000;
+			g++;
+			final int x = 20 + (g + 1) * 60;
+			arrowTimer.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					class ArrowRun implements Runnable {
+						int x;
+						public ArrowRun(int x) {
+							this.x = x;
+						}
+
+						@Override
+						public void run() {
+							group.removeView(arrow);
+							RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+									RelativeLayout.LayoutParams.WRAP_CONTENT,
+									RelativeLayout.LayoutParams.WRAP_CONTENT);
+							params.topMargin = (int) 50;
+							params.leftMargin = x;
+							arrow.setLayoutParams(params);
+							group.addView(arrow);
+							arrow.setVisibility(0);
+						}
+						
+					}
+					runOnUiThread(new ArrowRun(x));
+				}
+			}, time);
+		}
+	}
+
+	void genTone() {
+		double duration = 0; // seconds
+		for (Note n : song.getNotes().getNotes()) {
+			duration += n.getLength(); // TODO tempo!!
+		}
+		numSamples = (int) (duration * sampleRate);
+		double[] sample = new double[numSamples];
+		generatedSnd = new byte[2 * numSamples];
+		int j = 0;
+		for (Note n : song.getNotes().getNotes()) {
+			Log.d("", "Pitch: " + n.getPitch());
+			double freqOfTone = n.getPitch(); // hz
+			int numThisNote = (int) (n.getLength() * sampleRate);
+			Log.d("", "J is: " + j);
+			// fill out the array
+			for (int i = 0; i < numThisNote; ++i) {
+				sample[j] = Math.sin(2 * Math.PI * i
+						/ (sampleRate / freqOfTone));
+				j++;
+			}
+		}
+
+		// convert to 16 bit pcm sound array
+		// assumes the sample buffer is normalised.
+		int idx = 0;
+		for (final double dVal : sample) {
+			// scale to maximum amplitude
+			final short val = (short) ((dVal * 32767));
+			// in 16 bit wav PCM, first byte is the low order byte
+			generatedSnd[idx++] = (byte) (val & 0x00ff);
+			generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+		}
+		Log.wtf("", "Buffer size in bytes: " + numSamples);
+		makeTrack();
+	}
+
+	public void makeTrack() {
+		audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate,
+				AudioFormat.CHANNEL_OUT_DEFAULT,
+				AudioFormat.ENCODING_PCM_16BIT, 2 * numSamples,
+				AudioTrack.MODE_STATIC);
+		audioTrack.write(generatedSnd, 0, generatedSnd.length);
+		audioTrack.setPlaybackRate(8000);
 	}
 }
--- src/com/MusicalSketches/SongSelect.java
+++ src/com/MusicalSketches/SongSelect.java
@@ -35,102 +35,108 @@ import com.MusicalSketches.datarep.*;
 public class SongSelect extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.new_song_setting);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_song_setting);
 
-	    Spinner spinner = (Spinner) findViewById(R.id.spinner);
-	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-	            this, R.array.planets_array, android.R.layout.simple_spinner_item);
-	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-	    spinner.setAdapter(adapter);
-	    spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
-	    
-	    Spinner button1 = (Spinner) findViewById(R.id.button1);
-	    ArrayAdapter<CharSequence> adapterb = ArrayAdapter.createFromResource(
-	            this, R.array.clef_array, android.R.layout.simple_spinner_item);
-	    adapterb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-	    button1.setAdapter(adapterb);
-	    button1.setOnItemSelectedListener(new MyOnItemSelectedListener());
-	    
-	    Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
-	    ArrayAdapter<CharSequence> adapterc = ArrayAdapter.createFromResource(
-	            this, R.array.meter_array, android.R.layout.simple_spinner_item);
-	    adapterc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-	    spinner1.setAdapter(adapterc);
-	    spinner1.setOnItemSelectedListener(new MyOnItemSelectedListener());
-	    
-	    //Should go to Edit mode once that is done
-	    Button setButton = (Button) findViewById(R.id.button2);
-	    setButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-            	
-            	Date d = new Date();
-            	Song song = new Song(d);
-            	
-            	//Set Clef
-            	Spinner button1 = (Spinner) findViewById(R.id.button1);
-            	if (button1.getSelectedItem().toString().equalsIgnoreCase("Bass")){
-            		//Set to bass clef.  Bass is 0
-            		song.setClef(0);
-            	} else {
-            		//treble is 1.
-            		song.setClef(1);
-            	}
-            	
-            	//Set Tempo
-            	EditText tempo = (EditText) findViewById(R.id.editText2);
-            	song.setTempo(Integer.valueOf(tempo.getText().toString()));
-            	
-            	//Set Title
-            	EditText title = (EditText) findViewById(R.id.editText1);
-            	song.setTitle(title.getText().toString());
-            	
-            	//Set Meter
-            	Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
-            	if(spinner1.getSelectedItem().toString().length() >= 4){
-            		song.setMeter(12,8);
-            		Log.d("","Meter: " + spinner1.getSelectedItem().toString());
-            	} else {
-            		String str = spinner1.getSelectedItem().toString();
-            		Log.d("","Meter (should be less than 12/8): " + str);
-            		song.setMeter(Integer.valueOf(str.substring(0,1)), Integer.valueOf(str.substring(2,3)));
-            	}
-            	
-            	
-            	//Toast for testing attribute setting
-            	Toast.makeText(getApplicationContext(), tempo.getText().toString(), 2).show();
-            	Intent next = new Intent(SongSelect.this,EditModeLegacy.class);
-            	next.putExtra("song object", song);
+		Spinner spinner = (Spinner) findViewById(R.id.spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.planets_array,
+				android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+
+		Spinner button1 = (Spinner) findViewById(R.id.button1);
+		ArrayAdapter<CharSequence> adapterb = ArrayAdapter.createFromResource(
+				this, R.array.clef_array, android.R.layout.simple_spinner_item);
+		adapterb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		button1.setAdapter(adapterb);
+		button1.setOnItemSelectedListener(new MyOnItemSelectedListener());
+
+		Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+		ArrayAdapter<CharSequence> adapterc = ArrayAdapter
+				.createFromResource(this, R.array.meter_array,
+						android.R.layout.simple_spinner_item);
+		adapterc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner1.setAdapter(adapterc);
+		spinner1.setOnItemSelectedListener(new MyOnItemSelectedListener());
+
+		// Should go to Edit mode once that is done
+		Button setButton = (Button) findViewById(R.id.button2);
+		setButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+
+				Date d = new Date();
+				Song song = new Song(d);
+
+				// Set Clef
+				Spinner button1 = (Spinner) findViewById(R.id.button1);
+				if (button1.getSelectedItem().toString()
+						.equalsIgnoreCase("Bass")) {
+					// Set to bass clef. Bass is 0
+					song.setClef(0);
+				} else {
+					// treble is 1.
+					song.setClef(1);
+				}
+
+				// Set Tempo
+				EditText tempo = (EditText) findViewById(R.id.editText2);
+				if (!tempo.getText().toString().equals("")) {
+					song.setTempo(Integer.valueOf(tempo.getText().toString()));
+				} else {
+					song.setTempo(110);
+				}
+
+				// Set Title
+				EditText title = (EditText) findViewById(R.id.editText1);
+				song.setTitle(title.getText().toString());
+
+				// Set Meter
+				Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+				if (spinner1.getSelectedItem().toString().length() >= 4) {
+					song.setMeter(12, 8);
+					Log.d("", "Meter: " + spinner1.getSelectedItem().toString());
+				} else {
+					String str = spinner1.getSelectedItem().toString();
+					Log.d("", "Meter (should be less than 12/8): " + str);
+					song.setMeter(Integer.valueOf(str.substring(0, 1)),
+							Integer.valueOf(str.substring(2, 3)));
+				}
+
+				// Toast for testing attribute setting
+				Toast.makeText(getApplicationContext(),
+						tempo.getText().toString(), 2).show();
+				Intent next = new Intent(SongSelect.this, EditModeLegacy.class);
+				next.putExtra("song object", song);
 				startActivityForResult(next, 0);
-            }
-        });
-	    
-	    //Should go back to library
-	    Button cancelButton = (Button) findViewById(R.id.button3);
-	    cancelButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-            	//Intent next = new Intent(SongSelect.this,MusicalLibrary.class);
-				//startActivity(next);
+			}
+		});
+
+		// Should go back to library
+		Button cancelButton = (Button) findViewById(R.id.button3);
+		cancelButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
 				finish();
-            }
-        });
-	        
+			}
+		});
+
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		setResult(resultCode, data);
 		finish();
 	}
-	
-	
+
 	public class MyOnItemSelectedListener implements OnItemSelectedListener {
 
-	    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-	    }
+		public void onItemSelected(AdapterView<?> parent, View view, int pos,
+				long id) {
+		}
 
-	    public void onNothingSelected(AdapterView parent) {
-	      // Do nothing.
-	    }
+		public void onNothingSelected(AdapterView parent) {
+			// Do nothing.
+		}
 	}
 }
